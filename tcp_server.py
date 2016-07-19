170b80cab7fc92f990e071c71c399221ab155849
scott.lan <scott.lan@taisys.com>
2016-07-19 12:09:06 +0800

    add more log

--- tcp_server.py
+++ tcp_server.py
@@ -5,18 +5,20 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the address given on the command line
-server_address = ('127.0.0.1', 10000)
+server_address = ('127.0.0.1', 3333)
 sock.bind(server_address)
+sock.listen(0)
 print >>sys.stderr, 'starting up on %s port %s' % sock.getsockname()
-sock.listen(1)
 
 while True:
     print >>sys.stderr, 'waiting for a connection'
     connection, client_address = sock.accept()
+    client_host, client_port = connection.getpeername()
     try:
         print >>sys.stderr, 'client connected:', client_address
         while True:
             data = connection.recv(16)
+            data += "%s, %s\n" % (str(client_host), str(client_port))
             print >>sys.stderr, 'received "%s"' % data
             if data:
                 connection.sendall(data)
